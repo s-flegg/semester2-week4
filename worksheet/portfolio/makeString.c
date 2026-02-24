547327ed36d09930343ca45cf71859f50feedad6
s-flegg <161213652+s-flegg@users.noreply.github.com>
2026-02-24 14:56:22 +0000

    portfolio task

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Samuel Flegg
+Student ID: 202018944
 */
 
 #include <stdio.h>
@@ -10,8 +10,17 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+    char buffer[49];
+    char seperator[] = "-";
 
     // process the command-line data using appropriate string functions
+    for (int i=1; i<argc; i++) {
+        strcat(buffer, argv[i]);
+
+        if (i != argc - 1) { // prevents trailing seperator
+            strcat(buffer, seperator);
+        }
+    }
 
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
